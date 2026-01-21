eb39faa39609372ca08cd7cee758ef1a78a75d54
Balia077 <balaramd894@gmail.com>
2026-01-21 20:34:42 +0530

    fix: update font paths and adjust landing page styles

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const LandingPage = () => {
   return (
     <div className='w-full h-screen bg-zinc-900 pt-1'>
-      <div className='textStructure mt-35 px-14'>
+      <div className='textStructure mt-40 px-14'>
         <div className="masker font-[font-1]">
-            <h1 className='uppercase text-9xl leading-[6.2vw]'>We Create</h1>
+            <h1 className='uppercase text-[9vw] leading-[6.5vw]'>We Create</h1>
         </div>
         <div className="masker font-[font-1]">
-            <h1 className='uppercase text-9xl leading-[6.2vw]'>Eye Opening </h1>
+            <h1 className='uppercase text-[9vw] leading-[6.5vw]'>Eye-Opening </h1>
         </div>
         <div className="masker font-[font-1]">
-            <h1 className='uppercase text-9xl leading-[6.2vw]'>Presentations</h1>
+            <h1 className='uppercase text-[9vw] leading-[6.5vw]'>Presentations</h1>
         </div>
       </div>
     </div>
