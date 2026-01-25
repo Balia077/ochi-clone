8c6db88cf7d8e45c5d0952485e2ed153fb61aadb
Balia077 <balaramd894@gmail.com>
2026-01-25 12:01:29 +0530

    fix: adjust spacing in Marquee component for improved layout

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,14 +4,14 @@ import React from 'react'
 const Marquee = () => {
   return (
     <div className='w-full py-10 rounded-tr-3xl rounded-tl-3xl bg-[#004D43]'>
-      <div className='text border-t-2 border-b-2 mt-15 whitespace-nowrap overflow-hidden border-zinc-400 flex gap-20'>
-        <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 10}} className='uppercase text-[28vw] leading-[30vw] font-[font-1] -mt-[6.5vw] -mb-[3vw]'>
+      <div className='text border-t-2 border-b-2 mt-15 whitespace-nowrap overflow-hidden border-zinc-400 flex'>
+        <motion.h1 initial={{x: "0"}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 10}} className='uppercase text-[28vw] leading-[30vw] font-[font-1] -mt-[6.5vw] pr-20 -mb-[3vw]'>
           We are ochi
         </motion.h1>
-        <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 10}} className='uppercase text-[28vw] leading-[30vw] font-[font-1] -mt-[6.5vw] -mb-[3vw]'>
+        <motion.h1 initial={{x: "0"}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 10}} className='uppercase text-[28vw] leading-[30vw] font-[font-1] -mt-[6.5vw] pr-20 -mb-[3vw]'>
           We are ochi
         </motion.h1>
-        <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 10}} className='uppercase text-[28vw] leading-[30vw] font-[font-1] -mt-[6.5vw] -mb-[3vw]'>
+        <motion.h1 initial={{x: "0"}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 10}} className='uppercase text-[28vw] leading-[30vw] font-[font-1] -mt-[6.5vw] pr-20 -mb-[3vw]'>
           We are ochi
         </motion.h1>
       </div>
